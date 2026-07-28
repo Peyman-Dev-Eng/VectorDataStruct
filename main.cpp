7b888c76d9951792c1f0ce3be547e4dbe1254860
Peyman <2025julylyly@gmail.com>
2026-07-28 20:41:30 +0330

    V.1.0.0

--- main.cpp
+++ main.cpp
@@ -6,13 +6,6 @@
 #include <random>
 #include "VECTOR/Vector.h"
 using namespace std;
-random_device rd;
-mt19937 mt( rd() );
-
-double RandNumb() {
-    uniform_real_distribution<double> distribution( 1, 50 );
-    return distribution( mt );
-}
 
 int main() {
     vector<vector<int>> initVEC;
